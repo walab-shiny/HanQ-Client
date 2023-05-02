0cdb24366b18fe90c9fdc52b6d275bbe5ced4653
Songdabin <thdekqls1222@naver.com>
2023-05-02 11:50:43 +0900

    chore[#1] : analytics component separation

--- src/pages/EventListPage.js
+++ src/pages/EventListPage.js
@@ -1,44 +1,24 @@
 import { Helmet } from 'react-helmet-async';
 import { useEffect, useState } from 'react';
-import sumBy from 'lodash/sumBy';
 // @mui
-import { Card, Divider, Container, useTheme, Stack, CardContent, Typography } from '@mui/material';
-// _mock
-import _mock from '../_mock/_mock';
-// components
-import Scrollbar from '../components/scrollbar';
-
+import { Card, Container, CardContent, Typography } from '@mui/material';
 import { useSettingsContext } from '../components/settings';
-import { useTable } from '../components/table';
 // sections
-import { EventViewTableRow, EventViewTableToolbar } from '../sections/@dashboard/event/list';
-import { InvoiceAnalytic } from '../sections/@dashboard/invoice/list';
+import { Analytics } from '../sections/@dashboard/event/list';
 import { getEventList } from '../apis/event.ts';
-import { getTagList } from '../apis/tag';
 import CarouselCenterMode from '../sections/@dashboard/event/list/CarouselCenterMode';
 
 // ----------------------------------------------------------------------
 
-const getTagNameList = (tagList) => tagList.map((tag) => tag.name);
-
 export default function EventList() {
-  const theme = useTheme();
-
-  const { page, order, orderBy, rowsPerPage, setPage, onSort, onChangePage, onChangeRowsPerPage } = useTable();
-
   const { themeStretch } = useSettingsContext();
 
   const [eventData, setEventData] = useState([]);
 
-  const [tagNameList, setTagNameList] = useState([]);
-
   useEffect(() => {
     const fetchData = async () => {
       const eventList = await getEventList();
-      const tagList = await getTagList();
-      const tagNameList = getTagNameList(tagList);
       setEventData(eventList);
-      setTagNameList(tagNameList);
     };
     fetchData();
   }, []);
@@ -50,48 +30,7 @@ export default function EventList() {
       </Helmet>
 
       <Container maxWidth={themeStretch ? false : 'lg'}>
-        <Card sx={{ mb: 5 }}>
-          <Scrollbar>
-            <Stack
-              direction="row"
-              divider={<Divider orientation="vertical" flexItem sx={{ borderStyle: 'dashed' }} />}
-              sx={{ py: 2 }}
-            >
-              <InvoiceAnalytic
-                title="전체 이벤트 수"
-                total={100}
-                percent={100}
-                // price={sumBy(tableData, 'totalPrice')}
-                icon="ic:round-receipt"
-                color={theme.palette.info.main}
-              />
-
-              <InvoiceAnalytic
-                title="이벤트 참석자 수"
-                percent={100}
-                total={100}
-                icon="eva:clock-fill"
-                color={theme.palette.warning.main}
-              />
-
-              <InvoiceAnalytic
-                title="어제 접속자 수"
-                percent={100}
-                total={100}
-                icon="eva:bell-fill"
-                color={theme.palette.error.main}
-              />
-
-              <InvoiceAnalytic
-                title="오늘 접속자 수"
-                percent={100}
-                total={100}
-                icon="eva:file-fill"
-                color={theme.palette.success.main}
-              />
-            </Stack>
-          </Scrollbar>
-        </Card>
+        <Analytics />
       </Container>
 
       <Container maxWidth={themeStretch ? false : 'lg'}>
