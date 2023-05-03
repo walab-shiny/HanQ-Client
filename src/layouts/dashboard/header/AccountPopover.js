ac69d27af7c129dbbff0ef3ca53d5f783970a28b
Songdabin <thdekqls1222@naver.com>
2023-05-04 02:45:21 +0900

    fix[#1] : fix profile page

--- src/layouts/dashboard/header/AccountPopover.js
+++ src/layouts/dashboard/header/AccountPopover.js
@@ -87,7 +87,7 @@ export default function AccountPopover() {
         <CustomAvatar src={user?.picture} alt={user?.name} name={user?.name} />
       </IconButtonAnimate>
 
-      <MenuPopover open={openPopover} onClose={handleClosePopover} sx={{ width: 200, p: 0 }}>
+      <MenuPopover open={openPopover} onClose={handleClosePopover} sx={{ minWidth: 200, p: 0 }}>
         <Box sx={{ my: 1.5, px: 2.5 }}>
           <Typography variant="subtitle2" noWrap>
             {user?.name}
@@ -97,16 +97,6 @@ export default function AccountPopover() {
             {user?.email}
           </Typography>
         </Box>
-
-        <Divider sx={{ borderStyle: 'dashed' }} />
-
-        <Stack sx={{ p: 1 }}>
-          {OPTIONS.map((option) => (
-            <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </Stack>
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
