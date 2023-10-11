44f62d6cf51e3278c28128fd81f9c354d0b492eb
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-10-11 16:39:37 +0900

    feat: paging 처리

--- src/apis/participant.ts
+++ src/apis/participant.ts
@@ -1,7 +1,9 @@
 import axios from '../utils/axios';
 import { IParticipant } from '../types/participant.ts';
 
-export const getParticipantList = async (id: string) => {
-  const response = await axios.get(`/api/user/attend/${id}`);
+export const getParticipantList = async (id: string, page?: number, size?: number) => {
+  const defaultPage = 0;
+  const defaultSize = 500;
+  const response = await axios.get(`/api/user/attend/${id}?page=${page ?? defaultPage}&size=${size ?? defaultSize}`);
   return response.data as IParticipant[];
 };
