08bc082b512cf89d81dd5e99fa10605596998f28
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2023-12-03 20:57:29 +0900

    fix: 프로필 변경 시 이미지를 변경하지 않으면 이미지가 사라지는 오류 해결

--- src/apis/user.js
+++ src/apis/user.js
@@ -2,7 +2,7 @@ import axios from '../utils/axios';
 import { uploadImage } from './event.ts';
 
 export const updateUser = async (data) => {
-  const picture = data.picture ? await uploadImage(data.picture) : '';
+  const picture = typeof data.picture === 'string' ? data.picture : await uploadImage(data.picture);
   const response = await axios.post('/api/user/update', {
     likes: data.tags.map((tag) => tag.id),
     picture,
